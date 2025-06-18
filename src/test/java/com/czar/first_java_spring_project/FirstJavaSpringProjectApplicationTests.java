8c66939e5472a414490441e18933cd7cdcfb9b49
Cezar Fernando <cezarfbc@gmail.com>
2025-06-18 19:49:24 -0300

    first commit

---
+++ src/test/java/com/czar/first_java_spring_project/FirstJavaSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czar.first_java_spring_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJavaSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
